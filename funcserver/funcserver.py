8850d713356905f842649ffae8b8d1cb2f61f51c
root <root@static.78.83.251.148.clients.your-server.de>
2015-04-24 08:23:32 +0200

    references #43

--- funcserver/funcserver.py
+++ funcserver/funcserver.py
@@ -263,8 +263,8 @@ class StatsCollector(object):
         if self.stats is None: return
         return self.stats.timing(key, ms)
 
-    def gauge(self, key, n):
-        self.gauge_cache[key] = n
+    def gauge(self, key, n, delta=False):
+        self.gauge_cache[key] = (n, delta)
 
     def _collect_ramusage(self):
         self.gauge('resource.maxrss',
@@ -277,8 +277,8 @@ class StatsCollector(object):
         for k, v in self.cache.iteritems():
             p.incr(k, v)
 
-        for k, v in self.gauge_cache.iteritems():
-            p.gauge(k, v)
+        for k, (v, d) in self.gauge_cache.iteritems():
+            p.gauge(k, v, d)
 
         p.send()
         self.cache = {}
