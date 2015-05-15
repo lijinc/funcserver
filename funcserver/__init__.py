933a7d2144a2f93a57e906be8d15607922ec04fd
prashanthellina <prashanthellina@gmail.com>
2015-05-15 14:44:28 -0700

    fixes #47

--- funcserver/__init__.py
+++ funcserver/__init__.py
@@ -1,2 +1,2 @@
 from funcserver import FuncServer, RPCServer, BaseHandler, RPCClient, BaseScript, StatsCollector
-from funcserver import BaseHandle, make_handler, tag, mime, raw
+from funcserver import make_handler, tag, mime, raw
